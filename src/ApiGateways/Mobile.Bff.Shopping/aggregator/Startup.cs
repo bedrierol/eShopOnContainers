6d0981674efea3ac2bbe251a00b0c969ffbf3309
bedri erol <bedri.erol@evatro.com>
2021-08-09 13:44:41 +0300

    some changes to run in local

--- src/ApiGateways/Mobile.Bff.Shopping/aggregator/Startup.cs
+++ src/ApiGateways/Mobile.Bff.Shopping/aggregator/Startup.cs
@@ -39,11 +39,11 @@ namespace Microsoft.eShopOnContainers.Mobile.Shopping.HttpAggregator
         {
             services.AddHealthChecks()
                 .AddCheck("self", () => HealthCheckResult.Healthy())
-                .AddUrlGroup(new Uri(Configuration["CatalogUrlHC"]), name: "catalogapi-check", tags: new string[] { "catalogapi" })
+/*                .AddUrlGroup(new Uri(Configuration["CatalogUrlHC"]), name: "catalogapi-check", tags: new string[] { "catalogapi" })
                 .AddUrlGroup(new Uri(Configuration["OrderingUrlHC"]), name: "orderingapi-check", tags: new string[] { "orderingapi" })
                 .AddUrlGroup(new Uri(Configuration["BasketUrlHC"]), name: "basketapi-check", tags: new string[] { "basketapi" })
                 .AddUrlGroup(new Uri(Configuration["IdentityUrlHC"]), name: "identityapi-check", tags: new string[] { "identityapi" })
-                .AddUrlGroup(new Uri(Configuration["PaymentUrlHC"]), name: "paymentapi-check", tags: new string[] { "paymentapi" });
+                .AddUrlGroup(new Uri(Configuration["PaymentUrlHC"]), name: "paymentapi-check", tags: new string[] { "paymentapi" })*/;
 
             services.AddCustomMvc(Configuration)
                  .AddCustomAuthentication(Configuration)
